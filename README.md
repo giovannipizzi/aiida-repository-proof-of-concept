de232c6dafe8e9aff608075c4942703a279d87b9
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2020-04-08 07:48:49 +0200

    Adding implementation of a possible new-style AiiDA repository
    This leverages the disk-objectstore implementation, and has
    a benchmarking routine.

---
+++ README.md
@@ -0,0 +1,24 @@
+# aiida-repository
+
+A simple mock-up of a possible new implementation of the AiiDA repository,
+leveraging the disk-objectstore package.
+
+## Goal
+This is not meant to be a final implementation, but just a simple mock-up
+implementation to test the interface, and the performance.
+
+In particular, the most useful code is the `example_repository.py` file,
+that can be run to try to export an existing AiiDA "legacy" repository (AiiDA v1.1 and earlier) into a new format using the disk-objectstore.
+
+In order to run it, you can create a bash file with the following content:
+```bash
+#!/bin/bash
+./example_repository.py -U "DB_USERNAME" -D test_repo -P "YOURPWD" -c -r ~/.aiida/repositories/aiidadb/repository/ -C "$@"
+```
+
+Replace `DB_USERNAME` and `YOURPWD` with the user name and password of a user that can connect to PostgreSQL.
+`test_repo` can be changed to the name of a different database.
+
+**VERY IMPORTANT NOTE!** You need to create `test_repo` first. But, **most importantly**,
+note that this needs to be a test database, as this will be DROPPED by the tests inside
+`example_repository.py`.
---
+++ setup.py
@@ -0,0 +1,62 @@
+import os
+import io
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+modulename = 'aiida_repository'
+the_license = "The MIT license"
+
+# Get the version number in a dirty way
+folder = os.path.split(os.path.abspath(__file__))[0]
+fname = os.path.join(folder, modulename, '__init__.py')
+with open(fname) as init:
+    ns = {}
+    # Get lines that match, remove comment part
+    # (assuming it's not in the string...)
+    versionlines = [
+        l.partition('#')[0]
+        for l in init.readlines()
+        if l.startswith('__version__')
+    ]
+if len(versionlines) != 1:
+    raise ValueError("Unable to detect the version lines")
+versionline = versionlines[0]
+version = versionline.partition('=')[2].replace('"', '').strip()
+
+setup(
+    name=modulename,
+    description=
+    "An mock-up implementation of a new-style AiiDA repository using the disk-objectstore package",
+    url='http://github.com/giovannipizzi/aiida-repository',
+    license=the_license,
+    author='Giovanni Pizzi',
+    version=version,
+    install_requires=[
+        'sqlalchemy',
+        'psycopg2-binary',
+        'disk-objectstore'
+    ],
+    extras_require={
+        "testing": [
+            'click'
+        ],
+    },
+    packages=find_packages(),
+    # Needed to include some static files declared in MANIFEST.in
+    include_package_data=True,
+    keywords=[
+        'AiiDA', 'object store', 'repository', 'benchmark'
+    ],
+    long_description=io.open(os.path.join(folder, 'README.md'), encoding="utf-8").read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Framework :: AiiDA",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
